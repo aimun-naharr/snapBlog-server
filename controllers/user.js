a679ddb93a2361c1fab8e8c0c4cfb91c68ca2932
aimunNaharr <aimunnaharr@gmail.com>
2022-12-16 12:54:24 +0600

    login user controller fixed

--- controllers/user.js
+++ controllers/user.js
@@ -5,16 +5,18 @@ export const registerUser = async (req, res) => {
         try {
                 const { firstName, lastName, email, password, confirmPassword } = req.body;
                 const existingUser=await User.findOne({email: email})
+                
                 if(existingUser) {return res.status(400).json({msg: 'User already exist'})}
                 if (password !== confirmPassword) return "password didn't match to confirm password";
                 const salt = await bcrypt.genSalt();
                 const hashPass = await bcrypt.hash(password, salt);
-                const user = new User({ firstName, lastName, email, password: hashPass });
-                const savedUser = await user.save();
-                const token=jwt.sign({id: user._id}, process.env.JWT)
-                res.status(200).json(savedUser, token);
+                const newUser = new User({ firstName, lastName, email, password: hashPass });
+                const user = await newUser.save();
+             
+                const token=jwt.sign({id: newUser._id}, process.env.JWT)
+                res.status(200).json({user, token});
         } catch (error) {
-                res.status(500).send({ error: error.message });
+                res.status(500).json({ error: error.message });
         }
 };
 
@@ -23,9 +25,9 @@ export const login = async (req, res) => {
               
         try {
                 const user = await User.findOne({email: email});
-                if (!user){ return res.staus(400).json({ msg: "User does not exist" })}
+                if (!user) return res.status(400).json({ msg: "User does not exist" })
                 const isMatch = await bcrypt.compare(password, user.password);
-                if (!isMatch){ return res.staus(400).json({ msg: "Invalid credentials" });}
+                if (!isMatch) return res.status(400).json({ msg: "Invalid credentials" })
                 const token=jwt.sign({id: user._id}, process.env.JWT)
                 res.status(200).json({token,user});
         } catch (error) {
