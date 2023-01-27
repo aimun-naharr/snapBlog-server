7fb7e3278557f0c4a3251fd1190150a6fbf365f7
aimunNaharr <aimunnaharr@gmail.com>
2023-01-27 11:58:39 +0600

    updated

--- app.js
+++ app.js
@@ -20,8 +20,7 @@ app.get("/", (req, res) => {
    res.send("Route is working! YaY!");
    res.setHeader("Access-Control-Allow-Origin", "*")
 res.setHeader("Access-Control-Allow-Credentials", "true");
-res.setHeader("Access-Control-Max-Age", "1800");
-res.setHeader("Access-Control-Allow-Headers", "content-type");
+
 res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
  });
  app.use('/api/posts', postRoute)
