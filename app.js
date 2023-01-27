fb09eb498aac4c65a123025201edefd827604a3d
aimunNaharr <aimunnaharr@gmail.com>
2023-01-27 11:45:45 +0600

    cors issue fixed

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app=express()
 
 // middlewares
 const corsOptions ={
-  origin:'http://localhost:5173/', 
+  origin:'*', 
   credentials:true,            //access-control-allow-credentials:true
   optionSuccessStatus:200
 }
@@ -18,6 +18,11 @@ app.use(bodyParser.urlencoded({limit: '30mb', extended: true }))
 
 app.get("/", (req, res) => {
    res.send("Route is working! YaY!");
+   res.setHeader("Access-Control-Allow-Origin", "*")
+res.setHeader("Access-Control-Allow-Credentials", "true");
+res.setHeader("Access-Control-Max-Age", "1800");
+res.setHeader("Access-Control-Allow-Headers", "content-type");
+res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
  });
  app.use('/api/posts', postRoute)
  app.use('/api/auth', userRoute)
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const PORT=process.env.PORT|| 5000
+const PORT=process.env.PORT|| 8000
 
 
 const CONNECTION_URL=`mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.n3pgyaj.mongodb.net/?retryWrites=true&w=majority`
