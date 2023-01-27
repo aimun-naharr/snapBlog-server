6cd17aa6109b8da01a02b1648835675d7f89c4b3
aimunNaharr <aimunnaharr@gmail.com>
2023-01-27 11:21:43 +0600

    cors issue fixed

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ import cors from 'cors'
 const app=express()
 
 // middlewares
-app.use(cors())
+const corsOptions ={
+  origin:'http://localhost:5173/', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.use(bodyParser.json({limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({limit: '30mb', extended: true }))
 
